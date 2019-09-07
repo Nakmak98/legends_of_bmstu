fc63bc9d57748d00a260195972c23c2fdc081895
nakmak98 <nakmak1998@gmail.com>
2019-09-07 03:29:48 +0300

    demo Quill editor

---
+++ src/components/TaskEditor.vue
@@ -0,0 +1,91 @@
+<template>
+    <div id="app">
+        <vue-editor id="editor"
+                    useCustomImageHandler
+                    @image-added="handleImageAdded"
+                    @image-removed="handleImageRemoved"
+                    v-model="htmlForEditor">
+        </vue-editor>
+        <button @click="sendTask">Сохранить</button>
+    </div>
+</template>
+
+<script>
+    import { VueEditor } from "vue2-editor";
+    import Axios from "axios";
+    export default {
+        components: {
+            VueEditor
+        },
+
+        data() {
+            return {
+                htmlForEditor: "",
+                saved: false
+            };
+        },
+
+        methods: {
+            handleImageAdded: function(file, Editor, cursorLocation, resetUploader) {
+                console.log(file)
+                var formData = new FormData();
+                formData.append("image", file);
+                Axios
+                    .put('/img', {
+                    data: formData
+                    })
+                    .then(result => {
+                        let url = result.data.url;
+                        Editor.insertEmbed(cursorLocation, "image", url);
+                        resetUploader();
+                    })
+                    .catch(err => {
+                        console.log(err);
+                    });
+            },
+            handleImageRemoved: function (image) {
+                console.log(image);
+            //     Axios.delete('/img', {image: image})
+            //         .then(response => {
+            //            console.log(response)
+            //         })
+            //         .catch(error => {
+            //             console.log(error);
+            //         });
+            },
+            sendTask: function () {
+                console.log("sending")
+                // Axios.post('/task', {task: this.htmlForEditor})
+                //         .then(response => {
+                //            this.saved = true
+                //            console.log(response)
+                //         })
+                //         .catch(error => {
+                //             console.log(error);
+                //         });
+
+            }
+        }
+    };
+</script>
+<!--<template>-->
+<!--    <div id="app">-->
+<!--        <vue-editor v-model="content"></vue-editor>-->
+<!--    </div>-->
+<!--</template>-->
+
+<!--<script>-->
+<!--    import { VueEditor } from "vue2-editor";-->
+
+<!--    export default {-->
+<!--        components: {-->
+<!--            VueEditor-->
+<!--        },-->
+
+<!--        data() {-->
+<!--            return {-->
+<!--                content: "<h1>Some initial content</h1>"-->
+<!--            };-->
+<!--        }-->
+<!--    };-->
+<!--</script>-->
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
  <template>
   <div class="home">
     <img alt="Vue logo"  src="../assets/logo.png">
+    <TaskEditor></TaskEditor>
   </div>
+
 </template>
 
 <script>
 // @ is an alias to /src
 // import Axios from 'axios'
-
+import TaskEditor from "../components/TaskEditor";
 export default {
   name: 'home',
   data: function(){
     return{}
   },
+  components: {
+    TaskEditor
+  }
 
 }
 
