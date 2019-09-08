caab7cf2663f2ee7a5a95ab9da99661b266a5146
= <nakmak1998@gmail.com>
2019-09-09 00:33:13 +0300

    create carcas of withTeamPlayer component

---
+++ src/components/account/withTeamPlayer.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h2>Команда № {{team.team_id}}</h2>
+        <h2>{{team.team_name}}</h2>
+        типа табличка
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "withTeamPlayer",
+        props: {
+            team: Object
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
