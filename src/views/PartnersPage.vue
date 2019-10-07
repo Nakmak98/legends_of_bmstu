6d4e9399c433afd91d2e6ceb827f1cf68e301ad6
Leff <leosergkie@gmail.com>
2019-10-07 21:34:13 +0300

    Some layout fixes in PartnersPage.vue

--- src/views/PartnersPage.vue
+++ src/views/PartnersPage.vue
@@ -2,8 +2,10 @@
 	<div class="basic-block">
 		<h1>Наши партнеры</h1>
     	<a href="http://questreet.ru/"><img src="@/assets/ParntersLogo.png" class="ParntersLogo"></a>
+    	<br>
+    	<br>
     	<p class="ql-align-center PartnersText">
-    		Любишь квесты? <a href=""><b>Улица Квестов</b></a> ждет тебя!<br>Лучшие квесты в Москве по мнению организаторов. Всем участниками - купон на скидку!
+    		Любишь квесты? <b>Улица Квестов</b> ждет тебя! Лучшие квесты в Москве по мнению организаторов. Всем участниками - купон на скидку!
     	</p>
     </div>
 </template>
@@ -28,12 +30,8 @@
 .PartnersText
 {
 	font-size: 24px;
-	&>b
-	{
-		font-size: 24px;
-	}
 }
-.PartnersText>a>b
+.PartnersText>b
 {
 	font-size: 24px;
 }
