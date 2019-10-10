9a978f547157b450b26c799e6c4ba5791ce45997
= <nakmak1998@gmail.com>
2019-10-10 11:53:48 +0300

    alerts on all tolltips

--- src/views/moderator/AllTooltips.vue
+++ src/views/moderator/AllTooltips.vue
@@ -1,18 +1,20 @@
 <template>
     <div v-if="user.role === 'MODERATOR' || user.role === 'ADMIN'" class="basic-block">
         <h3>Подсказки</h3>
-        <div v-for="task in tasks" v-bind:key="task.task_id">
-            <h3>Задание № {{task.task_id}}</h3>
-            <div class="task-container">
-                <div class="task-card"
-                     v-for="hint in sorted_hints(task.hints)"
-                     @click="$router.push({name: 'edit_tooltip', params:{
+        <div v-if="tasks">
+            <div v-for="task in tasks">
+                <h3>Задание № {{task.task_id}}</h3>
+                <div class="task-container">
+                    <div class="task-card"
+                         v-for="hint in sorted_hints(task.hints)"
+                         @click="$router.push({name: 'edit_tooltip', params:{
                                 task_id: task.task_id,
                                 hint_id: hint.hint_id,
                                 html: hint.html,
                                 cost: hint.cost
                                 }})">
-                    {{hint.cost}}
+                        {{hint.cost}}
+                    </div>
                 </div>
             </div>
         </div>
@@ -36,17 +38,23 @@
                 return this.$store.state.user
             }
         },
+        created() {
+           alert('created tasks: ' + this.tasks)
+        },
         mounted() {
             this.request_hints()
+            alert('mounted tasks: ' + this.tasks)
         },
         methods: {
             request_hints() {
                 Axios
                     .get('/hint/all')
                     .then(response => {
+                        alert(response)
                         this.tasks = response.data
                     })
                     .catch(error => {
+                        alert(error)
                         if(error.response) {
                             new ErrorHandler(error.response, this)
                         } else {
