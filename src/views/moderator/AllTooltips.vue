68b595f82fd9c896615b2f797c93ae4786a03a28
= <nakmak1998@gmail.com>
2019-10-10 12:03:22 +0300

    add sort

--- src/views/moderator/AllTooltips.vue
+++ src/views/moderator/AllTooltips.vue
@@ -6,7 +6,7 @@
                 <h3>Задание № {{task.task_id}}</h3>
                 <div class="task-container">
                     <div class="task-card"
-                         v-for="hint in task.hints"
+                         v-for="hint in sorted_hints(task.hints)"
                          v-bind:key="hint.hint_id"
                          @click="$router.push({name: 'edit_tooltip', params:{
                                 task_id: task.task_id,
@@ -39,23 +39,17 @@
                 return this.$store.state.user
             }
         },
-        created() {
-           alert('created tasks: ' + this.tasks)
-        },
         mounted() {
             this.request_hints()
-            alert('mounted tasks: ' + this.tasks)
         },
         methods: {
             request_hints() {
                 Axios
                     .get('/hint/all')
                     .then(response => {
-                        alert(response)
                         this.tasks = response.data
                     })
                     .catch(error => {
-                        alert(error)
                         if(error.response) {
                             new ErrorHandler(error.response, this)
                         } else {
@@ -63,9 +57,9 @@
                         }
                     })
             },
-            // sorted_hints(hints) {
-            //     return hints.sort((a,b) => a.cost - b.cost)
-            // }
+            sorted_hints(hints) {
+                return hints.sort((a,b) => a.cost - b.cost)
+            }
         }
     }
 </script>
